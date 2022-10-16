e6123d5fa1465f38bbd2ce75d93dc95cf7aafdee
RAJVARDHAN-POUL <113845237+RAJVARDHAN-POUL@users.noreply.github.com>
2022-10-16 20:13:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function next(){
+    document.getElementById("mc").src="Minecraft2.png";
+    document.getElementById("btn").style.display="none";
+    document.getElementById("btn2").style.display="inline";
+
+}
+function previous(){
+    document.getElementById("mc").src="MINECRAFT.png";
+    document.getElementById("btn2").style.display="none";
+    document.getElementById("btn").style.display="inline";
+}
